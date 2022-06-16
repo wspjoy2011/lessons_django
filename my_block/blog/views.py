2835d1d41f4316635dc28c9f2eeba9c70a27ddd6
wspjoy2011 <wspjoy2011@gmail.com>
2022-06-16 20:30:58 +0300

    add comments

--- my_block/blog/views.py
+++ my_block/blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from taggit.models import Tag
 
 from .models import Post, Comment
+from .forms import CommentForm
 
 
-def post_list(request):
-    object_list= Post.published.all()
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug is not None:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+
     paginator = Paginator(object_list, 1)
     page = request.GET.get("page", 1)
     try:
@@ -16,7 +24,9 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
 
     context = {"posts": posts,
-               "page": page}
+               "page": page,
+               "tag": tag
+               }
     return render(request, "blog/post/list.html", context)
 
 
@@ -27,7 +37,24 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day,
                              )
-    comments = Comment.objects.filter(post=post.pk)
-    context = {"post": post, "comments": comments}
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    context = {
+        "post": post,
+        "comments": comments,
+        "comment_form": comment_form
+    }
     return render(request, "blog/post/detail.html", context)
 
